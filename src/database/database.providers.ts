8776df50a47e49f4ff5a0015b644ad7fe3f06dde
Wallblue <nrb.busquet@gmail.com>
2025-03-26 17:59:33 +0100

    Add database connection

---
+++ src/database/database.providers.ts
@@ -0,0 +1,26 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const port = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined
+      const sync = process.env.DB_SYNCHRONIZE === 'true'
+
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: port,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [
+            __dirname + '/../**/*.entity{.ts,.js}',
+        ],
+        synchronize: sync,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];

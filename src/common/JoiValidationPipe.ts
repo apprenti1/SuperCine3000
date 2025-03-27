778c3ba53e4a72eaa1015f91e8a643f01b2e1e49
Wallblue <nrb.busquet@gmail.com>
2025-03-27 14:19:47 +0100

    Add Joi validation pipe

---
+++ src/common/JoiValidationPipe.ts
@@ -0,0 +1,19 @@
+import { ArgumentMetadata, BadRequestException, InternalServerErrorException, PipeTransform } from "@nestjs/common";
+import { isError, ObjectSchema } from "joi";
+import { generateValidationErrorMessage } from "./generate-validation-message";
+
+export class JoiValidationPipe implements PipeTransform{
+    constructor(private schema: ObjectSchema) {}
+
+    async transform(value: any, metadata: ArgumentMetadata) {
+        try {
+            const validated = await this.schema.validateAsync(value)
+            return validated
+        } catch (error) {
+            if(isError(error))
+                throw new BadRequestException(generateValidationErrorMessage(error.details))
+            else
+                throw new InternalServerErrorException()
+        }
+    }
+}

a69a2d9903a99875a16db5f94f974cee273b1fd7
Wallblue <nrb.busquet@gmail.com>
2025-04-05 17:25:25 +0200

    Simplify all Joi importations

--- src/tokens/validation/list-tokens.schema.ts
+++ src/tokens/validation/list-tokens.schema.ts
@@ -1,4 +1,4 @@
-import * as Joi from "joi"
+import Joi from "joi"
 import { TokensType } from "src/common/enums/tokens-type.enum"
 import { PaginationRequest } from "src/common/validation/PaginationRequest"
 

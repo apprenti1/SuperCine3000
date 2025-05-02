b61d11a3158e3eee58f037d8b57e6f547dee74e5
Wallblue <nrb.busquet@gmail.com>
2025-05-02 18:14:44 +0200

    Replace typeorm with @nestjs/typeorm lib and configure it

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,29 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './auth/roles.guard';
 import { JwtService } from '@nestjs/jwt';
+import { RoomsModule } from './rooms/rooms.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Token } from './tokens/token.entity';
+import { Room } from './rooms/entities/room.entity';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
     TokensModule,
-    ConfigModule.forRoot({ isGlobal: true })
+    RoomsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.SYNCHRONIZE == 'true',
+      entities: [User, Token, Room]
+    })
   ],
   controllers: [AppController],
   providers: [AppService, JwtService, 

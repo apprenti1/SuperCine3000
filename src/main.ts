1f5d46b3ac09f3ce6b3747d0e3aeeba17b2f05a6
Wallblue <nrb.busquet@gmail.com>
2025-05-04 20:48:52 +0200

    Install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Supercine3000')
+    .setDescription("Ceci est l'API por le cinéma NothingBetterThanAL. C'est une API RESTful en Node.js et TypeScript dont le but est de permettre la gestion complète d'un cinéma.")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 

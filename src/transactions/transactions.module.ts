f81a836a311bdad1fe8e4762a4e0b105cc32c16c
Wallblue <nrb.busquet@gmail.com>
2025-05-04 17:33:08 +0200

    Add users module in transactions module imports

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { MoneyTransaction } from "./transaction.entity";
 import { TransactionsController } from "./transactions.controller";
 import { TransactionsService } from "./transactions.service";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([MoneyTransaction])],
+    imports: [TypeOrmModule.forFeature([MoneyTransaction]), UsersModule],
     controllers: [TransactionsController],
     providers: [TransactionsService],
     exports: [TransactionsService]

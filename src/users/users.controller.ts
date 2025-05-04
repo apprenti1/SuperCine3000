a5f37fe7e50aa8d0164684bd4b840fb47d57a82d
Wallblue <nrb.busquet@gmail.com>
2025-05-04 20:49:05 +0200

    Add documentation of user crud

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,9 +10,13 @@ import { PaginationRequest } from "src/common/validation/PaginationRequest";
 import { SetRoles } from "src/auth/decorators/setRoles.decorator";
 import { Roles } from "src/common/enums/roles.enum";
 import { Public } from "src/auth/decorators/public.decorator";
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { number, string } from "joi";
 import { ListingReturn } from "src/common/interfaces/listing-return.interface";
 import { User } from "./user.entity";
 
+@ApiTags('Users')
+@ApiBearerAuth()
 @Controller('users')
 export class UsersController{
     constructor(private readonly usersService: UsersService) {}
@@ -20,12 +24,23 @@ export class UsersController{
     @Get()
     @UsePipes(new JoiValidationPipe(listUsersValidation))
     @SetRoles(Roles.admin)
+    @ApiOperation({summary: "Liste tous les utilisateurs."})
+    @ApiQuery({name: 'username', example: 'michel', required: false, type: string, description: "Présente les utilisateurs avec le nom d'utilisateur donné."})
+    @ApiQuery({name: 'email', required: false, type: string, description: "Présente les utilisateurs avec l'email donné."})
+    @ApiQuery({name: 'walletMax', required: false, type: number, description: "Présente ceux avec une quantité d'argent inférieure à celle donnée."})
+    @ApiQuery({name: 'walletMin', required: false, type: number, description: "Présente ceux avec une quantité d'argent supérieure à celle donnée."})
+    @ApiQuery({name: 'wallet', required: false, type: number, description: "Présente ceux ayant la quantité d'argent donnée exactement."})
+    @ApiQuery({name: 'role', required: false, type: string, description: "Présente ceux ayant le rôle donné. Valeurs possibles : customer ou admin."})
+    @ApiQuery({name: 'page', required: false, type: number, description: "Définit le numéro de la page à afficher."})
+    @ApiQuery({name: 'limit', required: false, type: number, description: "Définit le nombre d'utilisateurs par page."})
     getUsers(@Query() queryParams: ListUsersParam & PaginationRequest) : Promise<ListingReturn<User>> {
         return this.usersService.findAll(queryParams)
     }
 
     @Get(':id')
     @UsePipes(new JoiValidationPipe(userIdValidation), ExistingUserPipe)
+    @ApiOperation({summary: "Présente un utilisateur donné."})
+    @ApiParam({name: 'id', required: true, description: "ID de l'utilisateur à récupérer.", example: 1,type: number})
     getUser(@Param() params: UserId){
         return this.usersService.findById(params.id)
     }
@@ -33,12 +48,15 @@ export class UsersController{
     @Post()
     @UsePipes(new JoiValidationPipe(createUserValidation), UniqueUserPipe)
     @Public()
+    @ApiOperation({summary: "Enregistre un nouvel utilisateur."})
     register(@Body() reqBody : CreateUserRequest){
         return this.usersService.createUser(reqBody)
     }
 
     @Patch(':id')
     @SetRoles(Roles.admin)
+    @ApiOperation({summary: "Modifie un utilisateur donné."})
+    @ApiParam({name: 'id', required: true, description: "ID de l'utilisateur à modifier.", example: 1, type: number})
     updateUser(
         @Param(new JoiValidationPipe(userIdValidation), ExistingUserPipe) params: UserId,
         @Body(new JoiValidationPipe(updateUserValidation)) reqBody: UpdateUserRequest
@@ -49,12 +67,15 @@ export class UsersController{
     @Delete(':id')
     @SetRoles(Roles.admin)
     @UsePipes(new JoiValidationPipe(userIdValidation), ExistingUserPipe)
+    @ApiOperation({summary: "Supprime un utilisateur donné."})
+    @ApiParam({name: 'id', required: true, description: "ID de l'utilisateur à supprimer.", example: 1,type: number})
     deleteUser(@Param() params: UserId){
         return this.usersService.deleteUser(params)
     }
 
     @Post('seed')
     @Public()
+    @ApiOperation({summary: "Fourni la base de données en utilisateurs."})
     seedUsers(){
         return this.usersService.seedUsers()
     }

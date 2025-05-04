fd3cd9fb6bed13a623a3cb6451ba98c153cb9802
Wallblue <nrb.busquet@gmail.com>
2025-05-04 20:09:26 +0200

    Remove wallet modification from PATCH users/

--- src/users/validation/update-user.schema.ts
+++ src/users/validation/update-user.schema.ts
@@ -1,13 +1,11 @@
 import Joi from "joi";
 
 export interface UpdateUserRequest {
-    username?: string,
-    email?: string,
-    transactionAmount?: number
+    username?: string
+    email?: string
 }
 
 export const updateUserValidation = Joi.object<UpdateUserRequest>({
     username: Joi.string().min(3),
-    email: Joi.string().email(),
-    transactionAmount: Joi.number() // Positive = credit, negative = payement
+    email: Joi.string().email()
 }).options({abortEarly: false})

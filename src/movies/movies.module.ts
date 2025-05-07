cba1a8471ab7693a8cbe6633bb3649e71dd93a6c
Wallblue <nrb.busquet@gmail.com>
2025-05-07 13:27:36 +0200

    Add /movies/:id/planning route

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,11 +1,12 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { MoviesController } from "./movies.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Movie } from "./movie.entity";
 import { MoviesService } from "./movies.service";
+import { ScreeningsModule } from "src/screenings/screening.module";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Movie])],
+    imports: [TypeOrmModule.forFeature([Movie]), forwardRef(() => ScreeningsModule)],
     controllers: [MoviesController],
     providers: [MoviesService],
     exports: [MoviesService]
